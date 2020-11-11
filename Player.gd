9789fed72caa389cd74d0cf290052d68263cf94d
Pedro Esser <pedro.esser@hotmail.com>
2020-11-11 19:57:50 +0000

    init

---
+++ Player.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+const ACCELERATION = 800
+const MAXSPEED = 80
+const FRICTION = 400
+var velocity = Vector2.ZERO
+var keys = []
+
+func my_init(k):
+	keys = k
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength(keys[0]) - Input.get_action_strength(keys[2])
+	input_vector.y = Input.get_action_strength(keys[1]) - Input.get_action_strength(keys[3])
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAXSPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, delta * FRICTION)
+		
+	velocity = move_and_slide(velocity)
+	
