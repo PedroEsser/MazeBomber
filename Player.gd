df05e39a64a33511881bf36ee2d8244fca49e866
Simão Correia <simaosaojose@gmail.com>
2020-12-01 14:13:57 +0000

    Move Animations

--- Player.gd
+++ Player.gd
@@ -6,6 +6,10 @@ const FRICTION = 400
 var velocity = Vector2.ZERO
 var keys = []
 
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
 func my_init(k, otherPlayers):
 	#for p in otherPlayers:
 	#	add_collision_exception_with(p)
@@ -18,8 +22,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAXSPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, delta * FRICTION)
 		
 	velocity = move_and_slide(velocity)
